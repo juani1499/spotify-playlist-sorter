152b3d76bda7b9a2af410c4608592da88e3e12ab
Juan <juanisaacs1499@gmail.com>
2023-07-18 13:47:51 -0400

    Asks user to input playlist id, and gives option to name the new playlist

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from spotipy.oauth2 import SpotifyOAuth
 import os
 
 # Set up Spotify client
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id='6a3d47087f3a4581bed907de4c13959d',
-                                               client_secret='24977ff97ba740b7b3bfcdbce1ace145',
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id='CLIENT_ID',
+                                               client_secret='CLIENT_SECRET',
                                                redirect_uri='http://localhost:3005/',
                                                scope='playlist-modify-public'))
 
@@ -20,20 +20,30 @@ def create_sorted_playlist(playlist_id):
     # Sort the tracks by BPM and key
     sorted_tracks = sorted(features, key=lambda f: (f['tempo'], f['key']))
 
+    # Prompt the user for the playlist name
+    playlist_name = input("What would you like to call the created playlist?: ")
+
+    # Create a new playlist
+    user_id = sp.me()['id']  # get the current user's ID
+    new_playlist = sp.user_playlist_create(user_id, playlist_name)
+
+    sp.playlist_add_items(new_playlist['id'], [track['id'] for track in sorted_tracks])
+
     with open('tracks.txt', 'w') as file:
         # Print the BPM and key of each track
         for track in sorted_tracks:
             file.write(f"Track ID: {track['id']}\n")
-            file.write(f"BPM:, {track['tempo']}")
+            file.write(f"BPM: {track['tempo']}\n")
             file.write(f"Key: {track['key']}\n")
             file.write('---\n')
 
     # Create a new playlist
     print("Track details written to tracks.txt")
+    print(f"Playlist '{playlist_name}' created successfully!")
 
 
 # Prompt the user for the playlist ID
-playlist_id = input("Enter the playlist ID: ")
+playlist_id = input("Enter the playlist ID you would like to mix: ")
 
 # Call the function to create the sorted playlist and write track details to a text file
 create_sorted_playlist(playlist_id)
