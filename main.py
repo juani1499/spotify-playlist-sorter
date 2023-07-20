666cbde11ea44b2af9d8b7275e5daf1625240a0c
Juan <juanisaacs1499@gmail.com>
2023-07-20 10:14:12 -0400

    Added a feature to include full track and artist name

--- main.py
+++ main.py
@@ -14,10 +14,15 @@ def create_sorted_playlist(playlist_id):
 
     # Get the audio features for each track
     tracks = playlist['tracks']['items']
-    features = sp.audio_features([track['track']['id'] for track in tracks])
+
+    track_ids = [track['track']['id'] for track in tracks]
+    track_names_artists_ids = [{**{'name': track['track']['name'],
+                                   'artist': track['track']['artists'][0]['name']},
+                                'id': track['track']['id']} for track in tracks]
+    features = sp.audio_features(track_ids)
 
     # Sort the tracks by BPM and key
-    sorted_tracks = sorted(features, key=lambda f: (f['tempo'], f['key']))
+    sorted_tracks = sorted(zip(track_names_artists_ids, features), key=lambda f: (f[1]['tempo'], f[1]['key']))
 
     # Prompt the user for the playlist name
     playlist_name = input("What would you like to call the created playlist?: ")
@@ -26,14 +31,15 @@ def create_sorted_playlist(playlist_id):
     user_id = sp.me()['id']  # get the current user's ID
     new_playlist = sp.user_playlist_create(user_id, playlist_name)
 
-    sp.playlist_add_items(new_playlist['id'], [track['id'] for track in sorted_tracks])
+    sp.playlist_add_items(new_playlist['id'], [track[0]['id'] for track in sorted_tracks])
 
     with open('tracks.txt', 'w') as file:
         # Print the BPM and key of each track
-        for track in sorted_tracks:
-            file.write(f"Track ID: {track['id']}\n")
-            file.write(f"BPM: {track['tempo']}\n")
-            file.write(f"Key: {track['key']}\n")
+        for track, feature in sorted_tracks:
+            file.write(f"Track Name: {track['name']}\n")
+            file.write(f"Artist: {track['artist']}\n")
+            file.write(f"BPM: {feature['tempo']}\n")
+            file.write(f"Key: {feature['key']}\n")
             file.write('---\n')
 
     # Create a new playlist
